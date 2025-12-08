ff2696db433b92d64ad55d0704cf2b5dd7a05c47
icekid smart <bellohabib682@gmail.com>
2025-12-08 01:41:37 +0100

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'lpres-admin-backend',
-      script: '/dist/server.js',
+      script: 'dist/server.js',
       instances: 1,
       exec_mode: 'fork',
       autorestart: true,
